71fa4c9728a4201d55e02563cba97548654a771c
DaniloRita <danilorita72@gmail.com>
2026-03-16 23:18:43 +0000

    atualizei o Readme

--- execícios/visaografica/app.js
+++ execícios/visaografica/app.js
@@ -1,4 +1,17 @@
-function menu(){
-    let meuMenu= document.getElementById("menu")
-    let myLista= document.getElementById("lista")
+function abrirmenu(){
+    let menu= document.getElementById("menu")
+     menu.classList.toggle("ativo");
+    if(menu.style.display === "none" || menu.style.display === ""){
+        menu.style.display = "block";
+    }
+    else{
+        menu.style.display="none"
+    }
+
+}
+function fecharMenu(){
+    let menu= document.getElementById("menu")
+    let fundo=document.getElementById("fundo")
+    menu.classList.remove("ativo")
+    fundo.style.display= "none"
 }
